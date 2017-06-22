299dada1dbc9b2febe9ccb4d227899686b49cfd2
congye6 <244053679@qq.com>
2017-06-22 18:24:47 +0800

    chart

--- app/src/main/java/layout/ScoreStatistic.java
+++ app/src/main/java/layout/ScoreStatistic.java
@@ -12,18 +12,23 @@ import android.widget.LinearLayout;
 
 import com.example.cong.helloworld.Controller.ScoreController;
 import com.example.cong.helloworld.R;
+import com.example.cong.helloworld.Tool.MyXFormatter;
 import com.example.cong.helloworld.vo.AllScore;
+import com.example.cong.helloworld.vo.StudentScore;
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ScoreStatistic.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ScoreStatistic#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ScoreStatistic extends Fragment {
 
 
@@ -31,14 +36,6 @@ public class ScoreStatistic extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ScoreStatistic.
-     */
     // TODO: Rename and change types and number of parameters
     public static ScoreStatistic newInstance() {
         ScoreStatistic fragment = new ScoreStatistic();
@@ -49,15 +46,17 @@ public class ScoreStatistic extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ScoreController controller=new ScoreController(this);
-        controller.getScores(38);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_score_statistic, container, false);
+        View view=inflater.inflate(R.layout.fragment_score_statistic, container, false);
+        ScoreController controller=new ScoreController(this);
+        controller.getScores(38);
+        return view;
     }
 
     public void setData(AllScore[] scores){
@@ -65,14 +64,66 @@ public class ScoreStatistic extends Fragment {
 
         if(scores.length==0){
             //显示无数据
+            return;
         }
         for(AllScore score:scores){
             LayoutInflater inflater=getActivity().getLayoutInflater();
             View view=inflater.inflate(R.layout.score_info,null);
-            LineChart chart=(LineChart) view.findViewById(R.id.chart);
+            BarChart chart=(BarChart) view.findViewById(R.id.first_chart);
 
+            //x轴
+            String[] scoreAreas={
+              "60以下","60-69","70-79","80-89","90-100"
+            };
+            List<String> x=new ArrayList<>();
+            for(String scoreArea:scoreAreas){
+                x.add(scoreArea);
+            }
 
+            //
+            int[] counts=new int[5];
+            Arrays.fill(counts,0);
+            for(StudentScore studentScore:score.students){
+                if(!studentScore.getScored().equals("true"))
+                    continue;
+                double num=studentScore.getScore();
+                if(num<60)
+                    counts[0]++;
+                if(num>=60&&num<70)
+                    counts[1]++;
+                if(num>=70&&num<80)
+                    counts[2]++;
+                if(num>=80&&num<90)
+                    counts[3]++;
+                if(num>=90&&num<=100)
+                    counts[4]++;
+            }
+            List<BarEntry> y=new ArrayList<>();
+            for(int i=0;i<5;i++){
+                BarEntry entry=new BarEntry(i,counts[i]);
+                y.add(entry);
+            }
 
+            IBarDataSet data=new BarDataSet(y,"分数");
+            BarData barData=new BarData(data);
+
+            chart.setData(barData);
+
+            XAxis xAxis = chart.getXAxis();
+            xAxis.setDrawAxisLine(false);
+            xAxis.setDrawGridLines(false);
+            MyXFormatter formatter=new MyXFormatter(scoreAreas);
+            xAxis.setValueFormatter(formatter);
+            chart.getAxisLeft().setDrawAxisLine(false);
+
+            chart.getDescription().setEnabled(false);//设置描述
+            chart.setPinchZoom(true);//设置按比例放缩柱状图
+            xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);//设置X轴标签显示位置
+
+            chart.setExtraBottomOffset(10);//距视图窗口底部的偏移，类似与paddingbottom
+            chart.setExtraTopOffset(30);//距视图窗口顶部的偏移，类似与paddingtop
+            chart.setFitBars(true);//使两侧的柱图完全显示
+            chart.animateX(1500);//数据显示动画，从左往右依次显示
 
             layout.addView(view);
         }
